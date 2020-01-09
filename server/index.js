31bc1e22ae67db6c680945374c4507c696e08c41
hamadanaoto <teiru_3@yahoo.co.jp>
2020-01-09 23:12:08 +0900

    Add http listen

--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 io.on('connection', socket => {
-    console.log('a user connected');
-    socket.broadcast.emit('hi');
+  console.log('a user connected');
+  socket.broadcast.emit('hi');
+
+  socket.on('sendMessage', (msg) => {
+    console.log(`message: ${msg}`);
+    io.emit('receiveMessage', { msg });
+  });
+});
+
+http.listen(3001, () => {
+  console.log('listening on http://localhost:3001');
 });
